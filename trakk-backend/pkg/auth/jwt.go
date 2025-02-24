3c7675a4fa71612463f28bc7277f81a29804a2ff
RonexLemon <ronexondimu@gmail.com>
2025-02-24 21:21:07 +0300

    verify token

--- trakk-backend/pkg/auth/jwt.go
+++ trakk-backend/pkg/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"log"
 	"time"
 	"trakk/config"
@@ -32,4 +33,26 @@ func CreateToken(username string,email string , phone string)(string,error){
 
 }
 
-func VerifyJwt
+func VerifyJwt(token string)(*jwt.Token, error){
+	secret, err := config.LoadJWTsecret()
+	if err != nil {
+		log.Fatal(err)
+		return nil,err
+		}
+		parsedToken, err:= jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+			
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(secret), nil
+		})
+		if err != nil {
+			return nil,err
+			}
+		if !parsedToken.Valid{
+			return nil,fmt.Errorf("invalid token")
+
+		}
+
+		return parsedToken,nil
+}
